92b2ffd6bfd6e11c3d41af54e6b9131511543854
Garben <877048038@qq.com>
2022-01-04 10:00:20 +0800

    启动输出

--- src/main/java/com/cygxtest/DemoApplication.java
+++ src/main/java/com/cygxtest/DemoApplication.java
@@ -11,6 +11,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("springboot启动成功！");
 	}
-
 }
